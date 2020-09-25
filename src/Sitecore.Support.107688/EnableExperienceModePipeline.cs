25f90f12f9280499f7b5dc6222a297fc01deebca
yevhen_povzlo <yevhen.povzlo@sitecore.com>
2020-09-25 16:08:33 +0300

    initial

---
+++ src/Sitecore.Support.107688/EnableExperienceModePipeline.cs
@@ -0,0 +1,47 @@
+﻿namespace Sitecore.Support.ExperienceExplorer.Business.Pipelines.HttpRequest.EnableExperienceModePipeline
+{
+    using Sitecore.Data;
+    using Sitecore.Diagnostics;
+    using Sitecore.Pipelines.HttpRequest;
+    using Sitecore.Sites;
+    using System;
+    using Sitecore.Web;
+    using Sitecore.ExperienceExplorer.Business.Helpers;
+    using Sitecore.ExperienceExplorer.Business.Managers;
+    using Sitecore.ExperienceExplorer.Business.Utilities;
+    using Sitecore.ExperienceExplorer.Business.Constants;
+
+    public class EnableExperienceModePipeline : HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            WebUtil.SetCookieValue(SettingsHelper.AddOnQueryStringKey, "0");
+            Assert.ArgumentNotNull(args, "args");
+            if (!SettingsHelper.ExperienceModePipelineEnabled)
+            {
+                return;
+            }
+            if (!SettingsHelper.IsEnabledForCurrentSite)
+            {
+                if (Context.Site.DisplayMode == DisplayMode.Normal)
+                {
+                    Context.Site.SetDisplayMode(DisplayMode.Edit, DisplayModeDuration.Remember);
+                }
+                return;
+            }
+            bool flag = PageModeHelper.IsExperienceMode || (PageModeHelper.HasPermission && (args.LocalPath == Paths.Local.Controls.Editor.AbsolutePath || args.LocalPath == Paths.Local.Controls.Viewer.AbsolutePath || args.LocalPath.StartsWith(Paths.Local.Services.AbsolutePath)));
+            string database = SiteContext.GetSite("website").SiteInfo.Database;
+            if (!string.IsNullOrEmpty(database))
+            {
+                bool flag2 = ModuleManager.IsExpButtonClicked && string.Compare(Context.Database.Name, database, StringComparison.InvariantCultureIgnoreCase) == 0;
+                if (flag || flag2)
+                {
+                    Database database2 = ExperienceExplorerUtil.ResolveContextDatabase();
+                    Context.Site.Database = database2;
+                    Context.Database = database2;
+                    WebUtil.SetCookieValue(SettingsHelper.AddOnQueryStringKey, "1");
+                }
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.107688/InjectExperienceExplorerControlPipeline.cs
@@ -0,0 +1,96 @@
+﻿namespace Sitecore.Support.ExperienceExplorer.Business.Pipelines.HttpRequest.EnableExperienceModePipeline
+{
+    using Sitecore.Configuration;
+    using Sitecore.Diagnostics;
+    using Sitecore.ExperienceExplorer.Business.Constants;
+    using Sitecore.ExperienceExplorer.Business.Helpers;
+    using Sitecore.ExperienceExplorer.Business.Managers;
+    using Sitecore.ExperienceExplorer.Business.Utilities;
+    using Sitecore.Pipelines.HttpRequest;
+    using Sitecore.Publishing;
+    using Sitecore.Sites;
+    using Sitecore.Web;
+    using System;
+    using System.Web;
+    using System.Web.UI;
+    using System.Web.UI.HtmlControls;
+
+    public class InjectExperienceExplorerControlPipeline 
+    {
+        public void Process(HttpRequestArgs args)
+        {
+            if (SettingsHelper.ExperienceModePipelineEnabled && Context.Item != null)
+            {
+                bool flag = ModuleManager.IsExpButtonClicked || PageModeHelper.IsExperienceMode;
+                if (flag && !ExperienceExplorerUtil.CurrentTicketIsValid())
+                {
+                    PageModeHelper.RedirectToLoginPage();
+                }
+                if (!Context.IsLoggedIn)
+                {
+                    PreviewManager.RestoreUser();
+                }
+                if (!Context.IsLoggedIn && WebUtil.GetQueryStringOrCookie(SettingsHelper.AddOnQueryStringKey) == "1")
+                {
+                    SiteContext site = Factory.GetSite("login");
+                    WebUtil.Redirect(site.VirtualFolder);
+                }
+                else if (SettingsHelper.IsEnabledForCurrentSite && Context.Site.DisplayMode == DisplayMode.Normal)
+                {
+                    try
+                    {
+                        WebUtil.SetCookieValue(SettingsHelper.AddOnQueryStringKey, flag ? "1" : "0");
+                        if (!flag)
+                        {
+                            goto IL_0142;
+                        }
+                        SettingsHelper.ExplorerWasAccessed = true;
+                        Control control = WebUtil.FindControlOfType(Context.Page.Page, typeof(HtmlForm));
+                        if (control != null)
+                        {
+                            Control child = Context.Page.Page.LoadControl(Paths.Module.Controls.GlobalHeaderPath);
+                            control.Controls.AddAt(0, child);
+                            Sitecore.ExperienceExplorer.Business.WebControls.ExperienceExplorer child2 = new Sitecore.ExperienceExplorer.Business.WebControls.ExperienceExplorer();
+                            control.Controls.Add(child2);
+                            ModuleManager.IsExpButtonClicked = false;
+                            HttpContext.Current.Items["IsExperienceMode"] = null;
+                            EnsureFirstLoad();
+                            if (!UserHelper.IsVirtualUser(Context.User.Name))
+                            {
+                                UserHelper.AuthentificateVirtualUser(Context.User.Name);
+                            }
+                            goto IL_0142;
+                        }
+                        goto end_IL_0083;
+                        IL_0142:
+                        if (Context.PageMode.IsPreview || Context.PageMode.IsPageEditor || Context.PageMode.IsDebugging)
+                        {
+                            UserHelper.AuthentificateRealUser();
+                        }
+                        end_IL_0083:;
+                    }
+                    catch (Exception ex)
+                    {
+                        if (!ex.Message.Contains("code blocks"))
+                        {
+                            Log.Error("Inject Experience Explorer Control: ", ex, this);
+                        }
+                    }
+                }
+            }
+
+        }
+        private void EnsureFirstLoad()
+        {
+            if (HttpContext.Current.Session["IsFirstTime"] == null && !UserHelper.IsVirtualUser(Context.User.Name))
+            {
+                HttpContext.Current.Session["IsFirstTime"] = true;
+            }
+            else
+            {
+                HttpContext.Current.Session["IsFirstTime"] = false;
+            }
+        }
+
+    }
+}
